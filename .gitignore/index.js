712b2dd089f2863adafc372f2fb74117e45ec5b3
Surimii <44616686+Surimii@users.noreply.github.com>
2018-10-30 21:47:13 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,7 +13,13 @@ bot.login(process.env.TOKEN);
 bot.on('message', message => {
     if (message.content === prefix + "help"){
         var embed = new Discord.RichEmbed()
-        .addField("Commands list: \n _help \n waifu \n Who are you? \n _serverinfo \n _poll", message.channel.sendMessage);
+        .setTitle("HELP")
+        .setDescription("Commands list:")
+        .addField("_help","Print this message", true)
+        .addField("_serverinfo","Print a resume of the server", true)
+        .addField("_poll","AnimeBot-chan create a poll for you", true)
+        .addField("waifu","Reveals AnimeBot-chan's true feelings", true)
+        .addField("Who are you?","AnimeBot-chan presents herserlf", true)
     message.channel.sendEmbed
     }
 
@@ -27,15 +33,15 @@ bot.on('message', message => {
     
     if (message.content === prefix + "serverinfo"){
         var embed = new Discord.RichEmbed()
-        .setDescription("Server's info")
-        .addField("Server's name", message.guild.name)
-        .addField("Server owner", message.guild.owner)
-        .addField("Server region", message.guild.region)
-        .setFooter('Server created at:')
-        .setTimestamp(message.guild.createdAt)
-        .addField("Joined at", message.member.joinedAt)
-        .addField("Users on the server (with bots)", message.guild.memberCount)
-        .setColor("0xFF459F")
+            .setDescription("Server's info")
+            .addField("Server's name", message.guild.name)
+            .addField("Server owner", message.guild.owner)
+            .addField("Server region", message.guild.region)
+            .addField('Server created at:')
+            .setTimestamp(message.guild.createdAt)
+            .addField("Joined at", message.member.joinedAt)
+            .addField("Users on the server (with bots)", message.guild.memberCount)
+            .setColor("0xFF459F")
     message.channel.sendEmbed(embed)
     }
     
