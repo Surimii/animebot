c66fd86217d21369086fb8503d3e88b45cb80eec
Surimii <44616686+Surimii@users.noreply.github.com>
2018-10-30 19:29:14 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("*")
+var prefix = ("_")
 
 bot.on('ready', function(){
     bot.user.setGame("with master");
@@ -12,11 +12,14 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n -*help");
+        message.channel.sendMessage("Liste des commandes: \n _help");
     }
 
-    if (message.content === "Salut"){
-        message.reply("Bien le bonjour. :)");
-        console.log("Commande Salut effectué");
+    if (message.content === "waifu"){
+        message.reply("Nadeko, no doubt");
+    }
+    
+    if (message.content === "who are you?"){
+        message.channel.sendMessage("I'm AnimeBot, nice to meet you ! ^^");
     }
 });
