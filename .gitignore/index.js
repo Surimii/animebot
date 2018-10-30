4f94a8405affde88da8a65672b09cc54478bc41a
Surimii <44616686+Surimii@users.noreply.github.com>
2018-10-30 20:18:43 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,14 +12,26 @@ bot.login(process.env.TOKEN);
 
 bot.on('message', message => {
     if (message.content === prefix + "help"){
-        message.channel.sendMessage("Liste des commandes: \n _help");
+        message.channel.sendMessage("Liste des commandes: \n _help \n waifu \n Who are you?");
     }
 
     if (message.content === "waifu"){
-        message.reply("Nadeko, no doubt");
+        message.reply("you're already my waifu <3");
     }
     
-    if (message.content === "who are you?"){
+    if (message.content === "Who are you?"){
         message.channel.sendMessage("I'm AnimeBot, nice to meet you ! ^^");
     }
+    
+    if (message.content === prefix + "info"){
+        var embed = new Discord.RichEmbed()
+        .setDescription("Information du Discord")
+        .addField("Nom du discord", message.guild.name)
+        .addField("Crée le", message.guild.createAt)
+        .addField("Tu as rejoins le", message.member.joinedAt)
+        .addField("Utilisateurs sur le discord", message.guild.memberCount)
+        .setColor("0x0000FF")
+    message.channel.sendEmbed(embed)
+    }
+    
 });
