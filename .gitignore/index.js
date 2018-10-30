455095df43cb79e115dd1fe4f36be89c6f6253db
Surimii <44616686+Surimii@users.noreply.github.com>
2018-10-30 19:17:12 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function(){
+    bot.user.setGame("Command: *help");
+    console.log("Connected");
+});
+
+bot.login(process.env.TOKEN);
+
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: \n -*help");
+    }
+
+    if (message.content === "Salut"){
+        message.reply("Bien le bonjour. :)");
+        console.log("Commande Salut effectué");
+    }
+});
